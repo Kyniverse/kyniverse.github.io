c25062a5bb093477741beda00eec585e40e2ba5b
Kyniverse <kenneth_vd_berg@outlook.com>
2025-04-05 17:34:32 +0000

    Deploying to gh-pages from @ Kyniverse/kyniverse.github.io@0d2964639358b483bb07134fa8eefaa1893fc4df 🚀

--- assets/index-Dwr_n8VR.js
+++
@@ -1,1 +0,0 @@
-import{t as n,i as e,c as t}from"./index-D8ZgPqg-.js";import{Header as o}from"./index-BgzRPTfD.js";var a=n("<main>");const m=l=>(()=>{var r=a();return e(r,t(o,{}),null),e(r,()=>l.children,null),r})(),p=()=>t(m,{children:"Hello World"});export{p as Home};
