0e1ee80786f7da37859dce06b3506c62060efae7
Kenneth van den Berg <kyniverse@gmail.com>
2025-04-14 16:38:11 +0200

    Updated Header

--- src/pages/dev/index.tsx
+++ src/pages/dev/index.tsx
@@ -39,7 +39,7 @@ export const Dev: Component = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d621.229270814075!2d5.683248282245107!3d51.47803626617841!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c72396d051d36f%3A0x5cf17b0589fb9a5c!2sWijkhuis%20de%20Lier!5e0!3m2!1snl!2snl!4v1744629858749!5m2!1snl!2snl" 
         height="360" 
         style="border:0; border-radius: 16px; box-shadow: 0px 0px 16px 0px rgba(0,0,0,0.1); margin: 32px; width: calc(100% - 64px);"
-        allowfullscreen="" 
+        allowfullscreen={true}
         loading="lazy" 
         referrerpolicy="no-referrer-when-downgrade">
 
