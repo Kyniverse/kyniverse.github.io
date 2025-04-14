3f2b269165e0f2908d57f4088ccc890df765edc9
Kenneth van den Berg <kyniverse@gmail.com>
2025-04-14 13:29:09 +0200

    Created header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,25 @@
-import { Component } from "solid-js"
+import { Component, onMount } from "solid-js"
 import { A } from "@solidjs/router";
 import "./style.scss";
 
 export const Header: Component = () => {
+  let _reference;
+
+  onMount(() => {
+    window.addEventListener('scroll', () => {
+      let _offset = window.pageYOffset;
+      let _threshold = _offset > (window.innerHeight * 0.66);
+      _reference.classList.toggle('visible', _threshold);
+    });
+  });
+
   return (
-    <header class="header">
+    <header class="header" ref={_reference}>
       <div class="wrapper">
         <h1 class="title">Goed Gestemd</h1>
         <nav class="navigation">
-          <A href="/" end={true}>HOME</A>
-          <A href="/" end={true}>ABOUT</A>
+          <A href="/" end={true} class="button">HOME</A>
+          <A href="/" end={true} class="button">ABOUT</A>
         </nav>
       </div>
     </header>
